5620b37ba26544e77f281f6e36876284a53b0ed3
yedoka <132571700+yedoka@users.noreply.github.com>
2026-03-29 16:08:42 +0200

    [LMS-002] Update environment variables and enhance server functionality with Socket.IO integration (#1)

--- index.ts
+++ index.ts
@@ -1,12 +1,41 @@
 import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+if (!PORT) {
+  throw new Error("PORT is not defined in the environment variables.");
+}
+
+if (!CLIENT_URL) {
+  throw new Error("CLIENT_URL is not defined in the environment variables.");
+}
 
 const app = express();
-const port = 8080;
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(
+  cors({
+    origin: CLIENT_URL,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}...`);
 });
